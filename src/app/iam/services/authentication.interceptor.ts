8fd30d01b73bdda0a1346d0d6bc8a08f490fa7a6
Angel Velasquez <pcisavel@upc.edu.pe>
2024-06-15 16:21:59 -0500

    refactor: transformed interceptor into functional interceptor.

--- src/app/iam/services/authentication.interceptor.ts
+++ src/app/iam/services/authentication.interceptor.ts
@@ -1,15 +1,10 @@
-import {Injectable} from "@angular/core";
-import {HttpEvent, HttpHandler, HttpInterceptor, HttpRequest} from "@angular/common/http";
+import {HttpEvent, HttpHandlerFn, HttpInterceptorFn, HttpRequest} from "@angular/common/http";
 import {Observable} from "rxjs";
 
-@Injectable()
-export class AuthenticationInterceptor implements HttpInterceptor {
-
-    intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+export const authenticationInterceptor: HttpInterceptorFn = (request: HttpRequest<any>, next: HttpHandlerFn): Observable<HttpEvent<unknown>> => {
       const token = localStorage.getItem('token');
       const handledRequest = token
         ? request.clone({ headers: request.headers.set('Authorization', `Bearer ${token}`) })
         : request;
-      return next.handle(handledRequest);
-    }
+      return next(handledRequest);
 }
